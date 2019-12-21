033426e263372ce5d3d7044ee3ed58b38e007866
unknown <dinabag@mail.ru>
2019-12-22 01:13:27 +0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Header from './Header/Header';
+import Content1 from './Content1/Content1';
+import Speakers from './Speakers/Speakers';
+import Schedule from './Schedule/Schedule';
+import Auditorium from './Auditorium/Auditorium';
+import Footer from './Footer/Footer';
+import { Element  } from "react-scroll";
 
-function App() {
+const App=()=> {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <Content1 />
+      <Element name='speakers'>
+      <Speakers />
+        </Element>
+      <Element name='schedule'>
+      <Schedule />
+        </Element>
+        <Element name='auditorium'>
+        <Auditorium />
+        </Element>
+      <Footer />
     </div>
   );
 }
